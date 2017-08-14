eaf4aa202a9b20e224bfe4e20940b98cebb0475c
Louis Suo <louis.suo@myob.com>
2017-08-14 10:51:59 +1000

    init repo

---
+++ generate.js
@@ -0,0 +1,14 @@
+module.exports = function () {
+  const faker = require('faker')
+  const _ = require('lodash')
+  return {
+    people: _.times(100, function (n) {
+      return {
+        id: n,
+        name: faker.name.findName(),
+        avatar: faker.internet.avatar(),
+        todo: faker.hacker.verb()
+      }
+    })
+  }
+}
